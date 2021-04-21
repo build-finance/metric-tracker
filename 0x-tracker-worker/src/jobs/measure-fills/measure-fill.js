a33c422af9298776b56bf78bd725f939693f2e79
SHA-2084 <2048.sha@gmail.com>
2021-04-21 17:16:37 +0200

    [bugfix] Fix trade value calculation error on unknown token

--- 0x-tracker-worker/src/jobs/measure-fills/measure-fill.js
+++ 0x-tracker-worker/src/jobs/measure-fills/measure-fill.js
@@ -1,9 +1,6 @@
-const bluebird = require('bluebird');
 
-const { BASE_TOKENS, BASE_TOKEN_DECIMALS } = require('../../constants');
 const formatTokenAmount = require('../../tokens/format-token-amount');
 const getConversionRate = require('../../rates/get-conversion-rate');
-const getMeasurableActor = require('./get-measurable-actor');
 const indexFillValue = require('../../index/index-fill-value');
 const indexFillTraders = require('../../index/index-fill-traders');
 const indexTradedTokens = require('../../index/index-traded-tokens');
@@ -13,60 +10,11 @@ const { logError } = require('../../util/error-logger');
 const getTokenMetadata = require('../../util/ethereum/get-token-metadata');
 
 const measureFill = async fill => {
-  const measurableActor = getMeasurableActor(fill);
 
-  let totalValue = 0;
-
-  await bluebird.mapSeries(fill.assets, async asset => {
-    if (asset.actor === measurableActor) {
-      const { tokenAddress } = asset;
-      const tokenMetadata = await getTokenMetadata(tokenAddress, {
-        rpcEndpoint: 'https://cloudflare-eth.com',
-      });
-
-      const tokenSymbol = tokenMetadata.symbol;
-      const tokenDecimals = tokenMetadata.decimals;
-
-      if (tokenSymbol === undefined) {
-        logError(
-          `Could not determine symbol for base token: ${tokenAddress}`, {}
-        );
-        return;
-      }
-
-      if (tokenDecimals === undefined) {
-        logError(
-          `Could not determine decimals for base token: ${tokenAddress}`,
-        );
-        return;
-      }
-
-      const tokenAmount = formatTokenAmount(asset.amount, tokenDecimals);
-      const normalizedSymbol = normalizeSymbol(tokenSymbol);
-      const tokenPrice = await getConversionRate(
-        tokenAddress,
-        normalizedSymbol,
-        'USD',
-        fill.date,
-      );
-
-      if (tokenPrice === undefined) {
-        logError(
-          `Unable to fetch USD price of ${normalizedSymbol} on ${fill.date}`,
-        );
-        return;
-      }
-
-      const tokenAmountUSD = tokenAmount * tokenPrice;
-
-      asset.set('price.USD', tokenPrice);
-      asset.set('value.USD', tokenAmountUSD);
-
-      if (totalValue === 0 && tokenAmountUSD > 0) {
-        totalValue += tokenAmountUSD;
-      }
-    }
-  });
+  let totalValue = await measureAssetValue(fill, fill.assets[0]);
+  if (totalValue === 0) {
+    totalValue = await measureAssetValue(fill, fill.assets[1]);
+  }
 
   if (totalValue > 0) {
     fill.set('conversions.USD.amount', totalValue);
@@ -80,6 +28,54 @@ const measureFill = async fill => {
     });
   }
 
+};
+
+const measureAssetValue = async (fill, asset) => {
+
+    const { tokenAddress } = asset;
+    const tokenMetadata = await getTokenMetadata(tokenAddress, {
+      rpcEndpoint: 'https://cloudflare-eth.com',
+    });
+
+    const tokenSymbol = tokenMetadata.symbol;
+    const tokenDecimals = tokenMetadata.decimals;
+
+    if (tokenSymbol === undefined) {
+      logError(
+          `Could not determine symbol for base token: ${tokenAddress}`, {}
+      );
+      return 0;
+    }
+
+    if (tokenDecimals === undefined) {
+      logError(
+          `Could not determine decimals for base token: ${tokenAddress}`,
+      );
+      return 0;
+    }
+
+    const tokenAmount = formatTokenAmount(asset.amount, tokenDecimals);
+    const normalizedSymbol = normalizeSymbol(tokenSymbol);
+    const tokenPrice = await getConversionRate(
+        tokenAddress,
+        normalizedSymbol,
+        'USD',
+        fill.date,
+    );
+
+    if (tokenPrice === undefined) {
+      logError(
+          `Unable to fetch USD price of ${normalizedSymbol} on ${fill.date}`,
+      );
+      return 0;
+    }
+
+    const tokenAmountUSD = tokenAmount * tokenPrice;
+
+    asset.set('price.USD', tokenPrice);
+    asset.set('value.USD', tokenAmountUSD);
+
+    return tokenAmountUSD
 };
 
 module.exports = measureFill;
